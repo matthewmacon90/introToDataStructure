dd67e132c47fa7a74804bbdf43acb1cbe862827a
Matthew Macon <matthewmacon90@gmail.com>
2023-02-02 12:23:57 -0500

    week1

--- code_examples.js
+++ code_examples.js
@@ -6,7 +6,7 @@ const pushExample = (num) => {
     return arrayOfNum.push(num);
 };
 pushExample(15);
-console.log('Push Example: ', arrayOfNum); //Not sure why when I call the function the num passed in is not saving.
+// console.log('Push Example: ', arrayOfNum); //Not sure why when I call the function the num passed in is not saving.
 
 //O(n);
 const loopExample = (num) => {
@@ -14,7 +14,7 @@ const loopExample = (num) => {
     for (let i = 0; i < arrayOfNum.length; i++) {
         if (i === lastIndex) {
             arrayOfNum.push(num);
-            console.log('Inside Loop: ', arrayOfNum);
+            // console.log('Inside Loop: ', arrayOfNum);
         };
     };
 };
@@ -22,12 +22,15 @@ loopExample(16);
 
 //O(logn);
 const loopSkip = (num) => {
-    const lastIndex = arrayOfNum.length;
-    for (let i = 1; i < arrayOfNum.length; i *= 2) {
-        console.log('This is loop');
-        console.log('I:', arrayOfNum[i]);
-        if (i < lastIndex) {                           // If block is not running because we are getting never equal to or greater than array.
-            console.log('This is If');
+    const arrayLength = arrayOfNum.length;
+    let numOfLoops = 0;
+    for (let i = 1; i < arrayOfNum.length; i *= 3) {
+        numOfLoops++
+        console.log('numOfLoops: ', numOfLoops);
+        // console.log('This is loop');
+        console.log('arrayOfNum:', arrayOfNum[i]);
+        if (arrayOfNum[i] < arrayLength) {                           // If block is not running because we are getting never equal to or greater than array.
+            console.log(`arrayOfNum: ${arrayOfNum[i]} is less than the length of arrayLength: ${arrayLength}`);
             arrayOfNum.push(num);
             console.log('O(logn) Loop: ', arrayOfNum);
         };
